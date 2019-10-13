1252765829deed1e7dca7ff2788ae801c7b03e3f
aruna <varuna148@gmail.com>
2019-10-13 18:49:44 +0530

    app8.java

---
+++ src/main/java/ericsson/App.java
@@ -0,0 +1,13 @@
+package ericsson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Good Morning everyone" );
+    }
+}
